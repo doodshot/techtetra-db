421d21e0ab4eca3f36d04fe093776e996b3445e1
doodshot <guido.panzieri@edu.itspiemonte.it>
2024-10-30 09:35:10 +0100

    fatto ultimo local storage

--- admin-login/admin-login.js
+++ admin-login/admin-login.js
@@ -93,3 +93,60 @@ document.getElementById('languageSelector').addEventListener('change', function(
             errorMessage.textContent = translationMap[savedLanguage] || errorMessage.textContent;
         }
     });
+    // funzione cambio lingua
+document.addEventListener('DOMContentLoaded', function() {
+    const savedLanguage = localStorage.getItem('language');
+    const email = document.getElementById('username');
+    const password = document.getElementById('password');
+    const loginButton = document.getElementById('login-button');
+    const forgotPassword = document.getElementById('forgot-password');
+    if (savedLanguage === 'it') {
+        email.placeholder = 'Email';
+        password.placeholder = 'Password';
+              loginButton.textContent = 'Login';
+              forgotPassword.textContent = 'Hai bisogno di assistenza?';
+              errorMessage.textContent = 'Credenziali Sbagliate';
+    } else if (savedLanguage === 'en') {
+        email.placeholder = 'Email';
+              password.placeholder = 'Password';
+              loginButton.textContent = 'Login';
+              forgotPassword.textContent = 'Do you need assistance?';
+              errorMessage.textContent = 'Wrong Credentials';
+    } else if (savedLanguage === 'fr') {
+        email.placeholder = 'Email';
+              password.placeholder = 'Password';
+              loginButton.textContent = 'Login';
+              forgotPassword.textContent = 'Avez-vous besoin d\'aide?';
+              errorMessage.textContent = 'Mauvaises Credentials';
+    } else if (savedLanguage === 'es') {
+        email.placeholder = 'Email';
+                password.placeholder = 'Password';
+                loginButton.textContent = 'Login';
+                forgotPassword.textContent = '¿Necesitas ayuda?';
+                errorMessage.textContent = 'Credenciales Incorrectas';
+    } else if (savedLanguage === 'ru') {
+        email.placeholder = 'электронная почта';
+            password.placeholder = 'пароль';
+            loginButton.textContent = 'авторизоваться';
+            forgotPassword.textContent = 'Вам нужна помощь?';
+            errorMessage.textContent = 'Неверные учетные данные';
+    } else if (savedLanguage === 'ch') {
+        email.placeholder = '电子邮件';
+            password.placeholder = '密码';
+            loginButton.textContent = '登录';
+            forgotPassword.textContent = '你需要帮助吗？';
+            errorMessage.textContent = '错误的凭证';
+    } else if (savedLanguage === 'dt') {
+        email.placeholder = 'Email';
+            password.placeholder = 'Passwort';
+            loginButton.textContent = 'Login';
+            forgotPassword.textContent = 'Benötigen Sie Hilfe?';
+            errorMessage.textContent = 'Falsche Anmeldeinformationen';
+    } else if (savedLanguage === 'tk') {
+        email.placeholder = 'e-posta';
+              password.placeholder = 'şifre';
+              loginButton.textContent = 'Giriş';
+              forgotPassword.textContent = 'Yardıma ihtiyacınız var mı?';
+              errorMessage.textContent = 'Yanlış kimlik bilgileri';
+    }
+});
